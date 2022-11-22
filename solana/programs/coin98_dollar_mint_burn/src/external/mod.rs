ce507348909758dabd8123034a69e55522ee2adb
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2022-11-22 14:19:21 +0700

    restructure

--- solana/programs/coin98_dollar_mint_burn/src/external/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod anchor_spl_token;
-pub mod chainlink_solana;
-pub mod cusd_token_mint;
-pub mod spl_associated_token;
-pub mod spl_token;
--- solana/programs/coin98_dollar_mint_burn/tests/utils/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod wallet;
-pub mod helper;
-pub mod chainlink;
-pub mod instructions;
